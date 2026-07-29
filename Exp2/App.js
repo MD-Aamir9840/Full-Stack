8ac203c679b857033800b487268e436d920ec59a
MD-Aamir9840 <aamirshekh9840@gmail.com>
2026-07-29 15:11:41 +0530

    Add files via upload

---
+++ Exp2/App.js
@@ -0,0 +1,14 @@
+// src/App.js
+import React from 'react';
+import CounterComponent from './features/counters/CounterComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CounterComponent />
+    </div>
+  );
+}
+
+export default App;
