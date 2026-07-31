ac9daaebfa8d681845824f3bbe7b40afa82c1878
MD-Aamir9840 <aamirshekh9840@gmail.com>
2026-07-31 13:10:26 +0530

    Delete Exp2 directory

--- Exp2/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counters/counterSlice';
-
-export const store = configureStore({
-  reducer: {
-    counters: counterReducer,
-  },
-});
